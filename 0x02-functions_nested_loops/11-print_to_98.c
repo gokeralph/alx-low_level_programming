738494f84bfe8777fcab4991850f9bb38800cf56
gokeralph <gokeralph@gmail.com>
2023-08-15 17:36:25 -0700

    print natural numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,6 +2,12 @@
 #include "6-abs.c"
 #include <stdio.h>
 
+/**
+ * print_to_98 - Prints all natural numbers from input to 98,
+ *		in order separated by a comma followed by a space.
+ * @n: The number to start the count.
+ */
+
 void print_to_98(int n)
 {
 	int i;
