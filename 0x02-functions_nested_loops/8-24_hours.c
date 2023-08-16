63508950840772377755a2f363a40a13c98a344c
gokeralph <gokeralph@gmail.com>
2023-08-15 17:06:53 -0700

    Jack Bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * jack_bauer - main thing
+ *
+ */
+void jack_bauer(void)
+{
+	int h;
+	int m;
+
+	for (h = 0; h <= 23; m++)
+	{
+		for (m = 0; m <= 59; m++)
+		{
+			_putchar(h / 10 + '0');
+			_putchar(h % 10 + '0');
+			_putchar(':');
+			_putchar(m / 10 + '0');
+			_putchar(m % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
