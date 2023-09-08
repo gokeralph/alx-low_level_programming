dbbbac4e062fd9f79f2fca2e2b4775f88b0e67d7
gokeralph <gokeralph@gmail.com>
2023-09-07 22:08:01 -0700

    0 fixed up

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -8,10 +8,12 @@
  */
 void *malloc_checked(unsigned int b)
 {
-    void *ptr = malloc(b);
-    if (ptr == NULL) {
-        fprintf(stderr, "Memory allocation failed\n");
-        exit(98);
-    }
-    return ptr;
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+	fprintf(stderr, "Memory allocation failed\n");
+	exit(98);
+	}
+	return (ptr);
 }
