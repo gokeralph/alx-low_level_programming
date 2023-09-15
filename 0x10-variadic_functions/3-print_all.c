8ae971cc0869e83d5193b4490304a1c43d461a25
gokeralph <gokeralph@gmail.com>
2023-09-15 04:35:34 -0700

    fixed proper

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,21 +2,41 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "variadic_functions.h"
+/**
+ * print_c - prints char
+ * @a: list to give
+ * Return: always 0
+ */
 int print_c(va_list a)
 {
 	printf("%c", va_arg(a, int));
 	return (0);
 }
+/**
+ * print_i - prints int
+ * @a: list to give
+ * Return: always 0
+ */
 int print_i(va_list a)
 {
 	printf("%d", va_arg(a, int));
 	return (0);
 }
+/**
+ * print_f - prints float
+ * @a: list to give
+ * Return: always 0
+ */
 int print_f(va_list a)
 {
 	printf("%f", va_arg(a, double));
 	return (0);
 }
+/**
+ * print_s - prints string
+ * @a: list to give
+ * Return: always 0
+ */
 int print_s(va_list a)
 {
 	char *s;
@@ -30,6 +50,11 @@ int print_s(va_list a)
 	printf("%s", s);
 	return (0);
 }
+/**
+ * print_all - prints all
+ * @format: format string that says arg types
+ *
+ */
 void print_all(const char * const format, ...)
 {
 	int i, j;
@@ -44,9 +69,9 @@ void print_all(const char * const format, ...)
 		{NULL, NULL}
 	};
 
-	va_start(anyArgs,format);
+	va_start(anyArgs, format);
 	i = 0;
-	while (format[i])
+	while (format != NULL && format[i])
 	{
 		j = 0;
 		while (ops[j].f != NULL)
@@ -62,5 +87,5 @@ void print_all(const char * const format, ...)
 		i++;
 	}
 	printf("\n");
-
+	va_end(anyArgs);
 }
