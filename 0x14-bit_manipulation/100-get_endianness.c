05a8a8b99a9365f880b44231bfb52037ef184052
gokeralph <gokeralph@gmail.com>
2023-09-29 03:16:01 -0700

    trying advance task

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* get_endianness - Determines if the machine is little or big endian.
+*
+* Return: 0 for big endian, 1 for little endian.
+*/
+
+int get_endianness(void)
+{
+unsigned int i = 1;
+char *c = (char *) &i;
+
+return (*c);
+}
